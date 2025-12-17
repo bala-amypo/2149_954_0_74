dfed195188b1db71d1a8b5185c51f2af958faf89
bala-amypo <bala@amypo.in>
2025-12-17 08:12:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -9,5 +9,22 @@ import com.example.demo.entity.Studentity;
 import com.example.demo.service.StudentService;
 @RestController
 @RequestMapping("/student")
-public class S
+public class StudentController{
+    @Autowired
+    private StudentService ser;
+
+    //post
+    @PostMapping("/add")
+    public Studentity addStudent(@RequestBody Studentity st){
+        return ser.saveData(st);
+    }
+    //GET ALL
+    @GetMapping("/getall")
+    public  Collection<Studentity>getAllstudents(){
+        return ser.getAll();
+    }
+    //GET BY ID
+    @GetMapping("/get/{id}")
+    public Studentity getStudentById(@)
+}
 
